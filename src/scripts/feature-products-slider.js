38aafe5559ab4a8b385bd42df8f220b45e90cc21
Chernikovmax <chernik.max@gmail.com>
2019-01-24 16:09:49 +0400

    minor bugfix for Feature product's gallery.

--- src/scripts/feature-products-slider.js
+++ src/scripts/feature-products-slider.js
@@ -1,17 +1,18 @@
 const first = document.querySelector('[data-type="1st-feature-product"]');
-first.addEventListener("click", showFeatureProduct(0));
+first.addEventListener("click", () => showFeatureProduct(0));
 
 const second = document.querySelector('[data-type="2nd-feature-product"]');
-second.addEventListener("click", showFeatureProduct(1));
+second.addEventListener("click", () =>showFeatureProduct(1));
 
 const third = document.querySelector('[data-type="3rd-feature-product"]');
-third.addEventListener("click", showFeatureProduct(2));
+third.addEventListener("click", () =>showFeatureProduct(2));
+
 
 const fourth = document.querySelector('[data-type="4th-feature-product"]');
-fourth.addEventListener("click", showFeatureProduct(3));
+fourth.addEventListener("click", () =>showFeatureProduct(3));
 
 const fifth = document.querySelector('[data-type="5th-feature-product"]');
-fifth.addEventListener("click", showFeatureProduct(4));
+fifth.addEventListener("click", () =>showFeatureProduct(4));
 
 function showFeatureProduct(index) {
   const products = document.getElementsByClassName('feature-products__slide');
