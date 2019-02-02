d125c54471636ac22547cb7c420d1375c67f0a42
Chernikovmax <chernik.max@gmail.com>
2019-02-02 14:46:47 +0400

    Redid the slideshow, implemented such functions like: play/pause (button), next and previous slide buttons and clicking on dots (which indicates the displayed slide) is playing certain slide and pauses playing. Have removed a node_modules path to implement ignoring function of it.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,25 +4,85 @@ import './feature-products-gallery.js';
 
 import './feature-products-comments.js';
 
+const slides = document.getElementsByClassName('slide');
+const dots = document.getElementsByClassName('slide-switches__dot');
+
 let slideIndex = 0;
+let slideInterval = setInterval(nextSlide, 2500);
+let play = true;
 
-showSlides();
+const playbackBtn = document.querySelector('.slide-switches__playback');
+const previousBtn = document.querySelector('.slide-switches__prev');
+const nextBtn = document.querySelector('.slide-switches__next');
 
-function showSlides() {
-  const slides = document.getElementsByClassName('slide');
-  const dots = document.getElementsByClassName('slide__dot-switcher');
+showFirstSlide();
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove('slide--active');
-    dots[i].classList.remove('slide__dot-switcher--active');
-  }
+function showFirstSlide() {
   slides[slideIndex].classList.add('slide--active');
-  dots[slideIndex].classList.add('slide__dot-switcher--active');
-  slideIndex++;
-  if (slideIndex > slides.length-1) {slideIndex = 0;}
-  setTimeout(showSlides, 3000);
+  dots[slideIndex].classList.add('slide-switches__dot--active');
 }
 
-function showCertainSlide(num) {
+function slideChange(n) {
+  clearSlideshow();
+  slideIndex = n;
+  if (slideIndex > slides.length-1) {
+    slideIndex = 0;
+    slides[slideIndex].classList.add('slide--active');
+    return dots[slideIndex].classList.add('slide-switches__dot--active');
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length-1;
+    slides[slideIndex].classList.add('slide--active');
+    return dots[slideIndex].classList.add('slide-switches__dot--active');
+  }
+  slides[slideIndex].classList.add('slide--active');
+  dots[slideIndex].classList.add('slide-switches__dot--active');
+}
 
+function nextSlide() {
+  slideChange(slideIndex+1);
+}
+
+function clearSlideshow() {
+  slides[slideIndex].classList.remove('slide--active');
+  dots[slideIndex].classList.remove('slide-switches__dot--active');
+}
+
+// The following code is for buttons
+
+function setSlide(num) {
+  pauseSlideshow();
+  slideChange(num);
+}
+global.setSlide = setSlide;
+
+function setPrevSlide() {
+  pauseSlideshow();
+  slideChange(slideIndex-1);
+}
+global.setPrevSlide = setPrevSlide;
+
+function setNextSlide() {
+  pauseSlideshow();
+  slideChange(slideIndex-1);
+}
+global.setNextSlide = setNextSlide;
+
+const playbackSwitcher = () => {
+  if (play === true) {return pauseSlideshow();}
+  if (play === false) {return playSlideshow();}
+};
+global.playbackSwitcher = playbackSwitcher;
+// playbackBtn.addEventListener('click', playbackSwitcher);
+
+function playSlideshow() {
+  play = true;
+  playbackBtn.innerHTML = '&#10074;&#10074;';
+  slideInterval = setInterval(nextSlide, 2500);
+}
+
+function pauseSlideshow() {
+  play = false;
+  playbackBtn.innerHTML = '&#9658';
+  clearInterval(slideInterval);
 }
