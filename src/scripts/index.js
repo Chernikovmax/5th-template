c5996472f84af8c93906a50c06351026c2f13d81
Maxim <chernik.max@gmail.com>
2019-02-28 15:43:36 +0400

    [MOD] Changed slideshow's nav bar icons (now it's svg) on homepage and JS-code (minor) for it, also changed scrollbar of all pages not just in gallery's modalbox. [FIX] Minor fixes in styles.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -79,13 +79,32 @@ global.playbackSwitcher = playbackSwitcher;
 
 function playSlideshow() {
   play = true;
-  playbackBtn.innerHTML = '&#9612;&#9612;';
+
+  playbackBtn.innerHTML =
+  `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0" y="0"
+     viewBox="0 0 47.607 47.607" xml:space="preserve">
+    <g>
+      <path d="M17.991,40.976c0,3.662-2.969,6.631-6.631,6.631l0,0c-3.662,0-6.631-2.969-6.631-6.631V6.631C4.729,2.969,7.698,0,11.36,0
+        l0,0c3.662,0,6.631,2.969,6.631,6.631V40.976z"/>
+      <path d="M42.877,40.976c0,3.662-2.969,6.631-6.631,6.631l0,0c-3.662,0-6.631-2.969-6.631-6.631V6.631
+        C29.616,2.969,32.585,0,36.246,0l0,0c3.662,0,6.631,2.969,6.631,6.631V40.976z"/>
+    </g>
+  </svg>`;
+
   slideInterval = setInterval(nextSlide, 2500);
 }
 
 function pauseSlideshow() {
   play = false;
-  playbackBtn.innerHTML = '&#9658';
+  playbackBtn.innerHTML =
+  `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0" y="0"
+  	 viewBox="0 0 232.153 232.153" xml:space="preserve">
+  	<g>
+  		<path style="fill: #ff2d2d;fill-rule:evenodd;clip-rule:evenodd;" d="M203.791,99.628L49.307,2.294c-4.567-2.719-10.238-2.266-14.521-2.266
+  			c-17.132,0-17.056,13.227-17.056,16.578v198.94c0,2.833-0.075,16.579,17.056,16.579c4.283,0,9.955,0.451,14.521-2.267
+  			l154.483-97.333c12.68-7.545,10.489-16.449,10.489-16.449S216.471,107.172,203.791,99.628z"/>
+  	</g>
+  </svg>`;
   clearInterval(slideInterval);
 }
 
