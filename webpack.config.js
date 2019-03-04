c8b23e0366274fd4fda925979860ecb86e0869f7
Maxim <chernik.max@gmail.com>
2019-03-04 21:46:52 +0400

    [ADD] Added game page. Started code. At the moment, you can only play with another person.

--- webpack.config.js
+++ webpack.config.js
@@ -10,19 +10,54 @@ const MiniCssExtractPluginConfig = new MiniCssExtractPlugin({
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CleanWebpackPluginConfig = new CleanWebpackPlugin(['dist']);
 
+const CopyPlugin = require('copy-webpack-plugin');
+const CopyPluginConfig = new CopyPlugin([
+  {
+    from: './src/imgs/',
+    to: 'media/imgs/',
+    toType: 'dir',
+  },
+  {
+    from: './src/icons/',
+    to: 'media/icons/',
+    toType: 'dir',
+  },
+  {
+    from: './src/favicon.ico',
+    to: 'media/favicon.ico',
+  },
+]);
+
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-
+  chunks: ['app'],
   template: './src/index.html',
   filename: 'index.html',
   inject: true,
   title: '5th-template',
+});
+const HtmlWebpackPluginConfigGallery = new HtmlWebpackPlugin({
+  chunks: ['gallery'],
+  template: './src/gallery.html',
+  filename: 'gallery.html',
+  inject: true,
+  title: 'Gallery',
+});
+const HtmlWebpackPluginConfigGame = new HtmlWebpackPlugin({
+  chunks: ['game'],
+  template: './src/game.html',
+  filename: 'game.html',
+  inject: true,
+  title: 'Game',
 });
 
 module.exports = {
   mode: 'development',
   entry: {
     app: './src/scripts/index.js',
+    gallery: './src/scripts/gallery/gallery.js',
+    game: './src/scripts/game/game.js',
   },
   output: {
     path: path.resolve('dist'),
@@ -45,8 +80,11 @@ module.exports = {
   plugins: [
     CleanWebpackPluginConfig,
     HtmlWebpackPluginConfig,
+    HtmlWebpackPluginConfigGallery,
+    HtmlWebpackPluginConfigGame,
     new webpack.HashedModuleIdsPlugin(),
     MiniCssExtractPluginConfig,
+    CopyPluginConfig
   ],
   devServer: {
     compress: true,
